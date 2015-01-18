1efb9f11304328ab1d0efbd4a2ada65a62ddd426
MalcolmK <malcolm.k.x@gmail.com>
2015-01-18 16:58:58 +0100

    Update settings

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,11 @@
+# Set path
 set -x PATH $HOME/.bin /bin /sbin /usr/local/bin /usr/local/sbin /usr/bin $PATH
 set -x PATH (brew --prefix coreutils)/libexec/gnubin $PATH
 set -x PATH /Users/Malcolm/pear/bin $PATH
 
+# Init boot2docker environment variables
 boot2docker shellinit | while read line; eval $line; end
 
-# Autojump
-# [[ -s $(brew --prefix)/etc/autojump.sh ]] && . $(brew --prefix)/etc/autojump.sh
+# Bitly credentials
+set bitly_api_key "R_14b4d42ad6a04c8f9811d5237e822391"
+set bitly_login "malcolmkindermans"
--- fish/functions/bitly.fish
+++ fish/functions/bitly.fish
@@ -1,28 +1,27 @@
 function bitly
-    # Set auth params
-    set api_key "R_14b4d42ad6a04c8f9811d5237e822391"
-    set login "malcolmkindermans"
-    set long_url $argv[1]
-
-    echo "long url: $long_url"
+    # Get long url
+    if test $argv[1]
+        set long_url $argv[1]
+    else
+        set long_url (pbpaste)
+    end
 
     # Create temp file to store curl result
     set tmpfile (mktemp -t bitly.XXXXX)
 
-    # Set the long url
-    set long_url $argv[1]
-
     # Perform the curl to bitly
     curl \
         --get \
-        --include "https://ismaelc-bitly.p.mashape.com/v3/shorten?apikey=$api_key&login=$login&longUrl=$long_url" \
+        --include "https://ismaelc-bitly.p.mashape.com/v3/shorten?apikey=$bitly_api_key&login=$bitly_login&longUrl=$long_url" \
         -H "X-Mashape-Key: GpxbctBCpqmshfFeehYND2bRnxjCp1desGYjsnmeHzQjTUktw7" \
-        -H "Accept: application/json" | grep 'status_code' >> $tmpfile
+        -H "Accept: application/json" | grep 'url' | jq '.data.url' | sed 's/\"//g' | sed ':a;N;$!ba;s/\n/,/g' >> $tmpfile
 
     set bitly_result (cat $tmpfile)
 
     # Delete temporary file
     rm -f $tmpfile
 
-    echo $bilty_result
+    # Set short url to clipboard
+    echo $bitly_result | pbcopy
+    echo $bitly_result
 end
