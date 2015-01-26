190fe5bbc1bdd932d0f5646f5564436baf628727
MalcolmK <malcolm.k.x@gmail.com>
2015-01-26 21:43:18 +0100

    Update settings

---
+++ fish/functions/fiks.fish
@@ -0,0 +1,13 @@
+function fiks
+	which php-cs-fixer
+	# Check if file location is passed as argument.
+	if test $argv[1]
+		set file_path $argv[1]
+	else
+		set file_path (pbpaste)
+	end
+
+	echo "Fixing file according to PSR-2."
+	php-cs-fixer fix "$file_path" --level=psr2
+	echo "File fixed."
+end
