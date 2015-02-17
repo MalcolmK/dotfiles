53e8d23ef218d36edb73425daf97b0a053af08ce
MalcolmK <malcolm.k.x@gmail.com>
2015-02-17 10:35:44 +0100

    Update settings

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,15 @@
+# > Variables
+# >> Set the Go Path
+set -x GOPATH $HOME/Development/GoCode
+# >> Set the db path for todo list
+set -x TODO_DB_PATH $HOME/.bash_todo_list/todo.json
+
 # > Set path
 set -x PATH $HOME/.bin /bin /sbin /usr/local/bin /usr/local/sbin /usr/bin $PATH
 set -x PATH (brew --prefix coreutils)/libexec/gnubin $PATH
 set -x PATH /Users/Malcolm/pear/bin $PATH
+# >> Add Go Path to path.
+set -x PATH $GOPATH/bin $PATH
 
 # Init boot2docker environment variables
 boot2docker shellinit | while read line; eval $line; end
