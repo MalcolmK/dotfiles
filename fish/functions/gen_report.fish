1933fb9fcaf716d8d425b657671b6e15cce19d32
MalcolmK <malcolm.k.x@gmail.com>
2015-05-18 10:59:24 +0200

    Update settings

--- fish/functions/gen_report.fish
+++ fish/functions/gen_report.fish
@@ -1,9 +1,26 @@
 function gen_report
+    if test $argv[1]
+        set times "$argv[1]"
+    else
+        set times 1
+    end
+
+    # Save the current directory
     pushd
+
+    # Goto the correct directory
 	afstuderen
 	cd docs/scriptie/
-	documark compile
+
+    # Compile the document
+    for time in (seq $times)
+	   documark compile
+    end
+
+    # Open the document
     set filename (documark config | jq '.filename' -r)
     open $filename
+
+    # Return to the old directory
     popd
 end
